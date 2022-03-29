7019e65050cda619a3a86bce5c0af884c3a5640c
Bastien Guyonnet <bastien.guyonnet@tutanota.com>
2022-03-29 10:09:34 +0200

    proxy upgraded

--- scripts/upgradeProxy.js
+++ scripts/upgradeProxy.js
@@ -1,8 +1,8 @@
 async function main() {
   //const fUSDTProxy = '0x512A00B3BbC54BAeefcf2FbD82E082E04bc5dffd';
-  const fraxProxy = '0x2a4a7B7AC87a416aE83772fEd196259A5fd47C63';
-  const stratFactory = await ethers.getContractFactory('ReaperAutoCompoundProtofiFarmer');
-  const stratContract = await hre.upgrades.upgradeProxy(fraxProxy, stratFactory);
+  const stratProxy = '0x34885918d804F46d53ea201BD5b9fdC44e0CD67B';
+  const stratFactory = await ethers.getContractFactory('ReaperAutoCompoundOxDao');
+  const stratContract = await hre.upgrades.upgradeProxy(stratProxy, stratFactory);
 }
 
 main()
