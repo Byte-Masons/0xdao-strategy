e07f506f8bdd4f8a690311f6ca131da72b10230b
Bastien Guyonnet <bastien.guyonnet@tutanota.com>
2022-04-09 00:22:52 +0200

    upgraded ftm-usdc

--- scripts/upgradeProxy.js
+++ scripts/upgradeProxy.js
@@ -1,6 +1,6 @@
 async function main() {
   //const fUSDTProxy = '0x512A00B3BbC54BAeefcf2FbD82E082E04bc5dffd';
-  const stratProxy = '0x34885918d804F46d53ea201BD5b9fdC44e0CD67B';
+  const stratProxy = '0xbF609F3cff33C1B65AbA9B851CA9E2223da39370';
   const stratFactory = await ethers.getContractFactory('ReaperAutoCompoundOxDao');
   const stratContract = await hre.upgrades.upgradeProxy(stratProxy, stratFactory);
 }
